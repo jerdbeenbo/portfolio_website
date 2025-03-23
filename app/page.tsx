9a91989a7be8077a64dbf205926cfdc3b5bd091e
Jarred Jenkins <jarredjenkins1000@gmail.com>
2025-03-23 18:42:31 +1000

    todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div>- Animejs hero section</div>
+      <div>- Display Search Visualiser</div>
+      <div>- Display elevator scheduler</div>
     </main>
   );
 }
